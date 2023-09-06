7a996626345b296b2b7e8ae47100926e16937239
snowflakepractice013 <144205767+snowflakepractice013@users.noreply.github.com>
2023-09-06 12:38:44 +0530

    Update streamlit_app.py
    changed a error for icon in breakfast menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 import streamlit
 
 streamlit.title('This first title')
-streamlit.header(🍞 'Breakfast Menu')
+streamlit.header('🍞 Breakfast Menu')
 streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥣Hard-Boiled Free-Range Egg')
