747ebaf4305c3a466e1de9f16060b8d9e613f1be
snowflakepractice013 <144205767+snowflakepractice013@users.noreply.github.com>
2023-09-06 12:38:10 +0530

    Update streamlit_app.py
    added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@
 import streamlit
 
 streamlit.title('This first title')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(🍞 'Breakfast Menu')
+streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥣Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
